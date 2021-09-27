6a69fb7cfe7eb686b1429fc1bc675a99fdb5bcb0
Pawan Theekshana <87897206+ImTheekshana126@users.noreply.github.com>
2021-09-27 07:22:32 +0530

    MissDewmiBot

---
+++ DewmiBot/modules/videoplay.py
@@ -0,0 +1,28 @@
+__help__ = """
+@szrosebot🇱🇰
+ ⚊❮❮❮❮  All Command List ❯❯❯❯⚊
+        
+✮ /vplay (reply to video or yt/live url) - to stream video
+✮ /vstop - stop the video streaming
+✮ /song (song name) - download song from YT
+✮ /vsong (video name) - download video from YT
+✮ /lyric (song name) - lyric scrapper
+✮ /vjoin - invite assistant join to your group
+✮ /vleave - order assistant leave from your group      
+             
+⚊❮❮❮❮ CHANNEL VIDEO PLAY ❯❯❯❯⚊    
+✮ /cvplay (reply to video or yt/live url) - to stream video
+✮ /cvstop - stop the video streaming
+
+⚊❮❮❮❮  EXTRA CMD ❯❯❯❯⚊
+✮ /tts (reply to text) - text to speech
+✮ /alive - check bot alive status
+✮ /sysinfo - check bot system information
+
+⚊❮❮❮❮ SUDO ONLY ❯❯❯❯⚊
+✮ /rmd - remove all downloaded files
+✮ /rmw - remove all downloaded raw files
+✮ /leaveall - order assistant leave from all group
+
+"""
+__mod_name__ = "video play "
